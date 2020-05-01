b49cc37e5a723e1a56bd49777f962e4ab4174cf0
yusunagai <yamikumoblue@hotmail.co.jp>
2020-05-01 13:49:48 +0900

    一問目終了

--- src/main/java/q001/Q001.java
+++ src/main/java/q001/Q001.java
@@ -7,8 +7,9 @@ package q001;
  * 正しく実行できるように修正してください。
  */
 public class Q001 {
-    void main() {
+    public static void main(String[] args)  {
         System.out.println("Hello World!");
     }
 }
-// 完成までの時間: xx時間 xx分
+// 完成までの時間: 0時間 2分
+// IDE設定、手順作成、検証などで2時間
