e267706f314146cbdc28ba0baae3fb479a3d9a16
yusunagai <yamikumoblue@hotmail.co.jp>
2020-05-01 17:03:55 +0900

    Q002完了。

--- src/main/java/q002/Q002.java
+++ src/main/java/q002/Q002.java
@@ -1,5 +1,8 @@
 package q002;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 /**
  * Q002 並べ替える
  *
@@ -45,5 +48,40 @@ public class Q002 {
             "14,林",
             "9,清水"
     };
+    // 実装部分
+    public static void main(String[] args)  {
+        String[] listData = dataList;
+        StrCompare c = new StrCompare();
+
+        Arrays.sort(listData, c);
+
+        for (String listDatum : listData) {
+            System.out.println(listDatum);
+        }
+    }
+
+    private static class StrCompare implements Comparator {
+        public int compare(Object a, Object b) {
+            if (a == b) return 0;
+            if (a instanceof String
+                    && b instanceof String) {
+
+                String[] sa = ((String) a).split(",");
+                String[] sb = ((String) b).split(",");
+
+                if (sa[0].length() == sb[0].length()) {
+                    return sa[0].compareTo(sb[0]);
+                } else if (sa[0].length() < sb[0].length()) {
+                    return -1;
+                } else {
+                    return 1;
+                }
+            } else {
+                throw new RuntimeException("Not String");
+            }
+        }
+    }
 }
-// 完成までの時間: xx時間 xx分
+// 完成までの時間: 1時間 11分
+// IDE上で実行結果が文字化ける⇒IntellJ周りの設定を見直し、さらにIDEが起動しない問題が発生
+// ユーザ名が日本語のため、日本語化のPleiadesを読みに行けなかった。ファイル置き場を変更、パスもかえて解決
